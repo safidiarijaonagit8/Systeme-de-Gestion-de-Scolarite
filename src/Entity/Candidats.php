93fbbffea8cc9b5f11fab1e62cc196ba41f0d903
Safidiarijaona <safidiarijaonagit8@gmail.com>
2023-10-10 15:10:13 +0300

    DEPL

---
+++ src/Entity/Candidats.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CandidatsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CandidatsRepository::class)]
+class Candidats
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $datedenaissance = null;
+
+    #[ORM\Column]
+    private ?float $moyennebacc = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getDatedenaissance(): ?\DateTimeInterface
+    {
+        return $this->datedenaissance;
+    }
+
+    public function setDatedenaissance(\DateTimeInterface $datedenaissance): static
+    {
+        $this->datedenaissance = $datedenaissance;
+
+        return $this;
+    }
+
+    public function getMoyennebacc(): ?float
+    {
+        return $this->moyennebacc;
+    }
+
+    public function setMoyennebacc(float $moyennebacc): static
+    {
+        $this->moyennebacc = $moyennebacc;
+
+        return $this;
+    }
+}
